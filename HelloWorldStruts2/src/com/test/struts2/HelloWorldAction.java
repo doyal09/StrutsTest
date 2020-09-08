e973195eb12c6e10212f56ef4cec4b040c87d07d
doyal09 <doyal.choudhury@aegon.co.uk>
2020-09-08 17:34:13 +0100

    Hello world struts

---
+++ HelloWorldStruts2/src/com/test/struts2/HelloWorldAction.java
@@ -0,0 +1,19 @@
+package com.test.struts2;
+
+public class HelloWorldAction {
+
+	   private String name;
+
+	   public String execute() throws Exception {
+	      return "success";
+	   }
+	   
+	   public String getName() {
+	      return name;
+	   }
+
+	   public void setName(String name) {
+	      this.name = name;
+	   }
+
+}
